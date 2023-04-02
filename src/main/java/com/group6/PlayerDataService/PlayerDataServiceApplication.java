36bcd7a91934867e1bf38f7f2b960c06efe13eea
Cameron <cameron_cintron@yahoo.com>
2023-04-02 18:26:32 -0500

    - add spring framework

---
+++ src/main/java/com/group6/PlayerDataService/PlayerDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.group6.PlayerDataService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerDataServiceApplication.class, args);
+	}
+
+}
