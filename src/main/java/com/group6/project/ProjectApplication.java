7727e3b55743c7b7f02e64e0dd29df2de25a968b
Cameron <cameron_cintron@yahoo.com>
2023-04-03 19:27:10 -0500

    - project files from SQL (did not include anything from concepts folder https://github.com/DePaul-KenYu/SE452/tree/sql-start/concept-demo/src/main/java/edu/depaul/cdm/se452/se452demo)

--- src/main/java/com/group6/project/ProjectApplication.java
+++ src/main/java/com/group6/project/ProjectApplication.java
@@ -1,13 +1,42 @@
 package com.group6.project;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
 public class ProjectApplication {
+
+	@Value("${environment}")
+	private String env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner showLogLevel() {
+		return (args) -> {
+			System.out.println(env);
+			log.debug("Debug");
+			log.info("Info");
+			log.warn("Warning");
+			log.error("Error");
+		};
+	}
+
+	/*@Bean
+	public CommandLineRunner showCourseList(CourseRepository repo) {
+		return (args) -> {
+			log.info("Num " + repo.count());
+		};
+	}*/
 
 }
