0725a7b287d4c6eb272b650b031bf18abdde6244
Cameron <cameron_cintron@yahoo.com>
2023-05-06 20:26:29 -0500

    - more trying to make cloud build work and not fail on unit tests

--- src/test/java/com/group6/project/relational/digitalassets/AccountInventoryTest.java
+++ src/test/java/com/group6/project/relational/digitalassets/AccountInventoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.sql.Date;
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@SpringBootTest
 public class AccountInventoryTest {
     @Autowired
     private AccountInventoryRepository accountInventoryRepository;
--- src/test/java/com/group6/project/relational/digitalassets/DigitalGoodsTest.java
+++ src/test/java/com/group6/project/relational/digitalassets/DigitalGoodsTest.java
@@ -3,12 +3,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@SpringBootTest
 public class DigitalGoodsTest {
     @Autowired
     private DigitalGoodsRepository digitalGoodsRepository;
