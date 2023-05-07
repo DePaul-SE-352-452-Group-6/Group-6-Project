51ddda8bc1ccf37756549a8aaac6f63487a8dafb
Cameron <cameron_cintron@yahoo.com>
2023-05-07 08:54:37 -0500

    milestone 3 - adding more detailed messages about service endpoints

--- src/main/java/com/group6/project/relational/account/AccountCurrencyService.java
+++ src/main/java/com/group6/project/relational/account/AccountCurrencyService.java
@@ -2,6 +2,8 @@ package com.group6.project.relational.account;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +16,14 @@ import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("/api/accounts/currencies")
+@Tag(name = "Account Currency", description = "All of the currencies owned by all of the accounts")
 @Log4j2
 public class AccountCurrencyService {
     @Autowired
     private AccountCurrencyRepository repo;
 
     @GetMapping
+    @Operation(summary = "Returns all the account currencies in the database")
     public List<AccountCurrency> list() {
         log.traceEntry("Enter list");
         var retval = repo.findAll();
@@ -28,6 +32,7 @@ public class AccountCurrencyService {
     }
 
     @PostMapping
+    @Operation(summary = "Save the currency for an account and returns the account currency id")
     public void save(@RequestBody AccountCurrency AccountCurrency) {
         log.traceEntry("enter save", AccountCurrency);
         repo.save(AccountCurrency);
@@ -36,6 +41,7 @@ public class AccountCurrencyService {
 
 
     @DeleteMapping
+    @Operation(summary = "Delete the account currency")
     public void delete(long code) {
         log.traceEntry("Enter delete", code);
         repo.deleteById(code);
--- src/main/java/com/group6/project/relational/account/AccountService.java
+++ src/main/java/com/group6/project/relational/account/AccountService.java
@@ -1,25 +1,34 @@
 package com.group6.project.relational.account;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("/api/accounts")
+@Tag(name = "Account", description = "All of the accounts created for this game")
 @Log4j2
 public class AccountService {
     @Autowired
     private AccountRepository repo;
 
     @GetMapping
+    @Operation(summary = "Returns all the account in the database")
+    @ApiResponse(responseCode = "200", description = "valid response",
+            content = {@Content(mediaType="application/json", schema=@Schema(implementation=Account.class))})
     public List<Account> list() {
         log.traceEntry("Enter list");
         var retval = repo.findAll();
@@ -28,6 +37,7 @@ public class AccountService {
     }
 
     @PostMapping
+    @Operation(summary = "Save the account and returns the account id")
     public void save(@RequestBody Account Account) {
         log.traceEntry("enter save", Account);
         repo.save(Account);
@@ -36,9 +46,22 @@ public class AccountService {
 
 
     @DeleteMapping
+    @Operation(summary = "Delete the account")
     public void delete(long code) {
         log.traceEntry("Enter delete", code);
         repo.deleteById(code);
         log.traceExit("Exit delete");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
