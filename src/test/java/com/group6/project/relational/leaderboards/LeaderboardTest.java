46dc846cc3b3d3a4ae7b91294df1ce33d1ede3d6
mhernandez193 <93899359+mhernandez193@users.noreply.github.com>
2023-05-14 20:50:44 -0500

    Add files via upload
    Leaderboard testing package

---
+++ src/test/java/com/group6/project/relational/leaderboards/LeaderboardTest.java
@@ -0,0 +1,23 @@
+package com.group6.project.relational.leaderboards;
+import com.group6.project.relational.leaderboards.*;
+import com.group6.project.relational.digitalassets.Currency;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LeaderboardTest {
+
+    @DisplayName("Test Create Account")
+    @Test
+    public void testAccount(){
+        Leaderboard entry = new Leaderboard();
+        entry.setID(1);
+        entry.setUserName("James");
+        entry.setAmount(10000);
+
+        String expectedNoError = "Leaderboard(id=1, userName=James, amount=10000)";
+        assertEquals(expectedNoError, entry.toString());
+    }
+}
